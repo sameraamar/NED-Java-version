98ccf29ee59a669805f610c288808c35b1c2ef5e
Samer Aamar <sameraamar@GSB144757.local>
2017-04-10 11:20:26 +0300

    Add run scripts

---
+++ run1.sh
@@ -0,0 +1,3 @@
+java -Xms9999m  -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=7896  -classpath ".:./bin:./lib/gson-2.6.2.jar:./lib/commons-pool2-2.4.2.jar:./lib/jedis-2.9.0.jar:./lib/spring-core-4.3.7.RELEASE.jar:./lib/spring-data-redis-1.8.1.RELEASE.jar" ned.main.AppMain -ifolder /Users/sameraamar/data -ofolder ~/temp -threads threads.txt -conf /Users/sameraamar/Thesis/NED-Java-version/conf.json
+
+
