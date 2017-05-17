fdb75b9c6fc75a32c5dc69c0151286e314519df1
t-samera <t-samera@microsoft.com>
2017-05-18 00:52:40 +0300

    fix parameters
    fix parameters

--- src/ned/types/GlobalData.java
+++ src/ned/types/GlobalData.java
@@ -40,14 +40,14 @@ public class GlobalData {
 		public int max_bucket_size = 2000;
 		public int max_documents = 50_000_000;
 		public int max_thread_delta_time = 4*3600; //seconds
-		public int offset = 200000;
+		public int offset = 0;
 		public int search_recents = 2000;
 		public double threshold = 0.5;
 		public double min_cluster_entropy = 0.0;
 		public double min_cluster_size = 1;
 		public int inital_dimension = 100000;
 		public int dimension_jumps = 50000;
-		public boolean resume_mode = true;
+		public boolean resume_mode = false;
 	}
 	
 	private static GlobalData globalData = null;
