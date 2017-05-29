0aab0ce8a8dc6c77845156538d0ce53ff9014f4a
ramid <ramid>
2017-05-29 22:27:21 +0300

    resume

--- src/ned/main/AppMain.java
+++ src/ned/main/AppMain.java
@@ -310,7 +310,7 @@ public class AppMain {
 	            if (processed == gd.getParams().max_documents)
 	            	stop = true;
 	            
-	            if (stop || processed % (gd.getParams().print_limit /2) == 0)
+	            if ((stop || processed % (gd.getParams().print_limit *10) == 0)/*&& false*/)
 	            {
 	            	int lastIndex = gd.resumeInfo.get(GlobalData.LAST_SEEN_IDX);
 	        		if(lastIndex <= idx)
@@ -319,7 +319,7 @@ public class AppMain {
 		            	while(!gd.getQueue().isEmpty())
 		            	{
 		            		try {
-		            			clustering.run();
+		            			clustering.resume();
 								Thread.sleep(50);
 							} catch (InterruptedException e) {
 								e.printStackTrace();
