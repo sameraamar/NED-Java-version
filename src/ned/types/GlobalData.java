a3528e6611b5565a8d246ae49054853d9c64903c
t-samera <t-samera@microsoft.com>
2017-05-30 20:28:29 +0300

    delta of time
    delta of time

--- src/ned/types/GlobalData.java
+++ src/ned/types/GlobalData.java
@@ -43,7 +43,7 @@ public class GlobalData {
 		public int hyperplanes = 13; // k  -->  2^k * 2000 --> 
 		public int max_bucket_size = 2000;
 		public int max_documents = 50_000_000;
-		public int max_thread_delta_time = 3600; //seconds
+		public int max_thread_delta_time = 4*3600; //seconds
 		public int offset = 0;
 		public int search_recents = 2000;
 		public double threshold = 0.65;
