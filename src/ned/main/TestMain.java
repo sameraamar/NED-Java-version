fa64c11396d389ff0167ac0e968b05fa895df667
t-samera <t-samera@microsoft.com>
2017-05-30 16:47:41 +0300

    delimiter
    delimiter

--- src/ned/main/TestMain.java
+++ src/ned/main/TestMain.java
@@ -1,6 +1,11 @@
 package ned.main;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 
 import ned.types.Document;
 import ned.types.GlobalData;
@@ -17,4 +22,28 @@ public class TestMain {
 		System.out.println(doc==null ? "NULL" : doc.toString());
 	}
 
+	
+	public void updateText()
+	{
+		String filename = "c:/temp/threads_7m_not_filtered.txt";
+		
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(filename));
+		    StringBuilder sb = new StringBuilder();
+		    String line = br.readLine();
+
+		    while (line != null) {
+		        sb.append(line);
+		        sb.append(System.lineSeparator());
+		        line = br.readLine();
+		    }
+		    String everything = sb.toString();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+		    br.close();
+		}
+	}
 }
