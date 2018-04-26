b6df9b19984f32f8661711a83c21e22bcc92928e
root <root@ip-172-31-14-249.us-west-2.compute.internal>
2018-04-26 21:29:42 +0000

    Unix scrpits

--- run2.sh
+++ run2.sh
@@ -1,25 +1,32 @@
 @echo off
 
-set DATAFILE_PATH=../data/demo
-set DATAFILE_PATH_AWS_BASE=s3://magnet-fwm/home/LiveU/joint_scenario
-set DATAFILE_PATH_AWS_INPUT=%DATAFILE_PATH_AWS_BASE%/raw_data/
-set DATAFILE_PATH_AWS_OUTPUT=%DATAFILE_PATH_AWS_BASE%/Bodof_Group/
+DATAFILE_PATH=../data/demo
+DATAFILE_PATH_AWS_BASE=s3://magnet-fwm/home/LiveU/joint_scenario
+DATAFILE_PATH_AWS_INPUT=$DATAFILE_PATH_AWS_BASE/Bodof_Group/data/
+raw_data/
+DATAFILE_PATH_AWS_OUTPUT=$DATAFILE_PATH_AWS_BASE/Bodof_Group/
 
-set DATAFILE_LOCAL=%DATAFILE_PATH%
+DATAFILE_LOCAL=$DATAFILE_PATH
 
-echo Downloading folder %DATAFILE_PATH_AWS_INPUT% from AWS ...
-mkdir %DATAFILE_LOCAL%
-aws s3 cp "%DATAFILE_PATH_AWS_INPUT%" "%DATAFILE_LOCAL%" --recursive --include "*.json"
+echo Downloading folder $DATAFILE_PATH_AWS_INPUT from AWS ...
+mkdir $DATAFILE_LOCAL
+##aws s3 cp s3://magnet-fwm/home/LiveU/joint_scenario/raw_data/ ../data/demo/ --recursive --include "*......xc*"
 
-echo "%DATAFILE_LOCAL%
+echo aws s3 cp "$DATAFILE_PATH_AWS_INPUT" "$DATAFILE_LOCAL" --recursive --include "\"*.gz\""
+
+aws s3 cp "$DATAFILE_PATH_AWS_INPUT" "$DATAFILE_LOCAL" --recursive --include "*.gz"
+
+"*.json"
+
+echo "DATAFILE_LOCAL"
 
 #java -XX:+UseG1GC -Xms50000m -classpath ".;./bin;./lib/gson-2.6.2.jar;./lib/commons-pool2-2.4.2.jar;./lib/jedis-2.9.0.jar;./lib/spring-core-4.3.7.RELEASE.jar;./lib/spring-data-redis-1.8.1.RELEASE.jar" ned.main.AppMain2
 
-java --Xmx120G  -Xss1024m -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=7896  -classpath ".:./bin:./lib/gson-2.6.2.jar:./lib/commons-pool2-2.4.2.jar:./lib/jedis-2.9.0.jar:./lib/spring-core-4.3.7.RELEASE.jar:./lib/spring-data-redis-1.8.1.RELEASE.jar" ned.main.AppMain2
+sudo java -Xmx120G  -Xss1024m -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=7896  -classpath ".:./bin:./lib/gson-2.6.2.jar:./lib/commons-pool2-2.4.2.jar:./lib/jedis-2.9.0.jar:./lib/spring-core-4.3.7.RELEASE.jar:./lib/spring-data-redis-1.8.1.RELEASE.jar" ned.main.AppMain2
 
 
 
-AWS s3 cp ../temp/threads_1000000_0/ %DATAFILE_PATH_AWS_OUTPUT% --recursive --include "*.csv"
-AWS s3 cp C:/temp/threads_1000000_0/ %DATAFILE_PATH_AWS_OUTPUT% --recursive --include "*.txt"
+aws s3 cp ../temp/threads_1000000_0/ $DATAFILE_PATH_AWS_OUTPUT --recursive --include "*.csv"
+aws s3 cp ../temp/threads_1000000_0/ $DATAFILE_PATH_AWS_OUTPUT --recursive --include "*.txt"
 
 
