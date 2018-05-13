2822883ecbea9bbfef4b2266fc9afdb5ab8cd0fe
root <root@ip-172-31-14-249.us-west-2.compute.internal>
2018-05-13 17:27:25 +0000

    script

--- run2.sh
+++ run2.sh
@@ -2,21 +2,26 @@
 
 DATAFILE_PATH=../data/demo
 DATAFILE_PATH_AWS_BASE=s3://magnet-fwm/home/LiveU/joint_scenario
-DATAFILE_PATH_AWS_INPUT=$DATAFILE_PATH_AWS_BASE/Feldman_Group/tweets_feldman_108k_glo.txt
+DATAFILE_PATH_AWS_INPUT=$DATAFILE_PATH_AWS_BASE/Feldman_Group/
+FILE_NAME=tweets_feldman_108k_loc.txt
+DATAFILE_PATH_AWS_INPUT=$DATAFILE_PATH_AWS_BASE/Feldman_Group/$FILE_NAME
 tweets_feldman_loc.txt
 DATAFILE_PATH_AWS_OUTPUT=$DATAFILE_PATH_AWS_BASE/Bodof_Group/
 
 DATAFILE_LOCAL=$DATAFILE_PATH
 
+rm -rf $DATAFILE_LOCAL
+
+
 echo Downloading folder $DATAFILE_PATH_AWS_INPUT from AWS ...
 mkdir $DATAFILE_LOCAL
 ##aws s3 cp s3://magnet-fwm/home/LiveU/joint_scenario/raw_data/ ../data/demo/ --recursive --include "*......xc*"
 
-echo aws s3 cp "$DATAFILE_PATH_AWS_INPUT" "$DATAFILE_LOCAL" --recursive --exclude "*" --include "*.*"
-aws s3 cp "$DATAFILE_PATH_AWS_INPUT" "$DATAFILE_LOCAL" --recursive --exclude "*" --include "*.*"
+echo aws s3 cp "$DATAFILE_PATH_AWS_INPUT" "$DATAFILE_LOCAL" --recursive --exclude "*" --include "$FILE_NAME"
+sudo aws s3 cp "$DATAFILE_PATH_AWS_INPUT" "$DATAFILE_LOCAL" --recursive --exclude "*" --include "$FILE_NAME"
 
-echo aws s3 cp "$DATAFILE_PATH_AWS_INPUT" "$DATAFILE_LOCAL" 
-aws s3 cp "$DATAFILE_PATH_AWS_INPUT" "$DATAFILE_LOCAL" 
+echo s3 cp "$DATAFILE_PATH_AWS_INPUT" "$DATAFILE_LOCAL/$FILE_NAME" 
+sudo aws s3 cp "$DATAFILE_PATH_AWS_INPUT" "$DATAFILE_LOCAL/$FILE_NAME" 
 
 echo "DATAFILE_LOCAL"
 
@@ -25,7 +30,14 @@ echo "DATAFILE_LOCAL"
 sudo java -Xmx120G  -Xss1024m -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=7896  -classpath ".:./bin:./lib/gson-2.6.2.jar:./lib/commons-pool2-2.4.2.jar:./lib/jedis-2.9.0.jar:./lib/spring-core-4.3.7.RELEASE.jar:./lib/spring-data-redis-1.8.1.RELEASE.jar" ned.main.AppMain2
 
 
+echo aws s3 cp ../temp/threads_1000000_0/ $DATAFILE_PATH_AWS_OUTPUT --recursive --exclude "\"*\"" --include "\"*.csv\""
+
 aws s3 cp ../temp/threads_1000000_0/ $DATAFILE_PATH_AWS_OUTPUT --recursive --exclude "*" --include "*.csv"
+remaws s3 cp ../temp/threads_1000000_0/ s3://magnet-fwm/home/LiveU/joint_scenario/Bodof_Group/ --recursive --exclude "*" --include "*.csv"
+remaws s3 cp ../temp/threads_1000000_0/ s3://magnet-fwm/home/LiveU/joint_scenario/Bodof_Group/ --recursive --exclude "*" --include "*.txt"
+
+echo aws s3 cp ../temp/threads_1000000_0/ $DATAFILE_PATH_AWS_OUTPUT --recursive --exclude "\"*\"" --include "\"*.txt\""
+
 aws s3 cp ../temp/threads_1000000_0/ $DATAFILE_PATH_AWS_OUTPUT --recursive --exclude "*" --include "*.txt"
 
 
