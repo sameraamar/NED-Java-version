b5fd9bc4fc817a865b02e786c09368d2f0e285b8
sameraamar <samer.aamar@gmail.com>
2018-05-13 17:10:58 +0300

    scriptscript
    script

--- run2.sh
+++ run2.sh
@@ -2,7 +2,8 @@
 
 DATAFILE_PATH=../data/demo
 DATAFILE_PATH_AWS_BASE=s3://magnet-fwm/home/LiveU/joint_scenario
-DATAFILE_PATH_AWS_INPUT=$DATAFILE_PATH_AWS_BASE/Feldman_Group/tweets_feldman_loc.txt
+DATAFILE_PATH_AWS_INPUT=$DATAFILE_PATH_AWS_BASE/Feldman_Group/tweets_feldman_108k_glo.txt
+tweets_feldman_loc.txt
 DATAFILE_PATH_AWS_OUTPUT=$DATAFILE_PATH_AWS_BASE/Bodof_Group/
 
 DATAFILE_LOCAL=$DATAFILE_PATH
@@ -22,7 +23,6 @@ echo "DATAFILE_LOCAL"
 #java -XX:+UseG1GC -Xms50000m -classpath ".;./bin;./lib/gson-2.6.2.jar;./lib/commons-pool2-2.4.2.jar;./lib/jedis-2.9.0.jar;./lib/spring-core-4.3.7.RELEASE.jar;./lib/spring-data-redis-1.8.1.RELEASE.jar" ned.main.AppMain2
 
 sudo java -Xmx120G  -Xss1024m -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=7896  -classpath ".:./bin:./lib/gson-2.6.2.jar:./lib/commons-pool2-2.4.2.jar:./lib/jedis-2.9.0.jar:./lib/spring-core-4.3.7.RELEASE.jar:./lib/spring-data-redis-1.8.1.RELEASE.jar" ned.main.AppMain2
-
 
 
 aws s3 cp ../temp/threads_1000000_0/ $DATAFILE_PATH_AWS_OUTPUT --recursive --exclude "*" --include "*.csv"
